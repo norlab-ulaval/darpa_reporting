d87aad2eaf4cc789ddfc97cd9d5ad343d324ef09
Alexandre Guénette <alexandre.guenette.1@ulaval.ca>
2021-07-21 11:51:25 -0400

    Add parameters validation

--- src/NodeParameters.cpp
+++ src/NodeParameters.cpp
@@ -3,6 +3,7 @@
 NodeParameters::NodeParameters(ros::NodeHandle privateNodeHandle)
 {
     retrieveParameters(privateNodeHandle);
+    validateParameters();
 }
 
 void NodeParameters::retrieveParameters(const ros::NodeHandle& nodeHandle)
@@ -15,4 +16,32 @@ void NodeParameters::retrieveParameters(const ros::NodeHandle& nodeHandle)
     nodeHandle.param<float>("map_publish_rate", mapPublishRate, 0.1);
     nodeHandle.param<float>("poses_publish_rate", posesPublishRate, 0.1);
     nodeHandle.param<int>("transform_dimension", transformDimension, 4);
+}
+
+void NodeParameters::validateParameters() const
+{
+    if (robotFrames.empty())
+        throw std::runtime_error("Empty robot frames");
+
+    if (darpaFrame.empty())
+        throw std::runtime_error("Empty darpa frame");
+
+    if (mapTopic.empty())
+        throw std::runtime_error("Empty map topic");
+
+    if (mapPublishTopic.empty())
+        throw std::runtime_error("Empty map publish topic");
+
+    if (posesPublishTopic.empty())
+        throw std::runtime_error("Empty poses publish topic");
+
+    if (mapPublishRate <= 0 || mapPublishRate > 1)
+        throw std::runtime_error("Invalid map publish rate: " + std::to_string(mapPublishRate));
+
+    if (posesPublishRate <= 0 || posesPublishRate > 1)
+        throw std::runtime_error("Invalid poses publish rate: " + std::to_string(posesPublishRate));
+
+    if (transformDimension != 3 && transformDimension != 4)
+        throw std::runtime_error(
+            "Invalid transform dimension: Must be 3 for 2D point cloud or 4 for 3D point cloud");
 }
